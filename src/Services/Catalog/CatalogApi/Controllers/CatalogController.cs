a10160b8c7e1f8086d65f0e2a949bbe54f270936
maria Abbasi <mariazulfi4@yahoo.com>
2023-03-02 08:22:24 +0500

    push

---
+++ src/Services/Catalog/CatalogApi/Controllers/CatalogController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CatalogApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[]")]
+    public class CatalogController
+    {
+    }
+}
---
+++ src/Services/Catalog/CatalogApi/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Entities;
+using CatalogApi.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
